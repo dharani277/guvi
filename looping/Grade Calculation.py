71b288a43094dcf0fc69cd1c7b5105a8a8d01369
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-13 05:20:08 +0530

    Create Grade Calculation.py

---
+++ looping/Grade Calculation.py
@@ -0,0 +1,17 @@
+# Grade Calculation by using Mark
+
+mark=int(input("Enter the Mark: "))
+if(mark>=90 and mark<=100):
+ print("Grade S")
+elif(mark>=80 and mark<=89):
+ print("Grade A")
+elif(mark>=70 and mark<=79):
+ print("Grade B")
+elif(mark>=60 and mark<=69):
+ print("Grade C")
+elif(mark>=55 and mark<=59):
+ print("Grade D")
+elif(mark>=50 and mark<=54):
+ print("Grade E")
+else:
+  print("Fail")
