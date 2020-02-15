682b868ab3f41e92475d8729cf626356bb69569f
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-15 15:27:28 +0530

    Create addition_of_two_number.py

---
+++ codekata/absolute_beginner/addition_of_two_number.py
@@ -0,0 +1,14 @@
+# addition of two numbers
+# get the two numbers as input from the user
+a=float(input())
+b=float(input())
+# create function for addition
+def add():
+# add and round of the value
+  c=round(a+b)
+# return the value c 
+  return c
+# call the function and store the output in s
+s=add()
+# print the value s
+print(s)
