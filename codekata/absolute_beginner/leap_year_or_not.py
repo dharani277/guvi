20d120a30cb0cece37ff602f183fcaf35d9060d8
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-15 22:08:45 +0530

    Create leap_year_or_not.py

---
+++ codekata/absolute_beginner/leap_year_or_not.py
@@ -0,0 +1,19 @@
+# leap year or not
+# get input value from user
+value=int(input())
+# define leap year function
+def leap_year():
+# if condition check whether the value is leap year
+  if(value%4==0):
+    c="Y"
+# reurn leap year
+    return c
+# else condition check whether the value is not a leap year
+  else:
+    c="N
+# return not leap year
+    return c
+# call the leap year function and store it in output
+output=leap_year()
+# print output 
+print(output)
