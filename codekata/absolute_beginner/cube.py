87e556272442e63e7c7999068117884acd2cc173
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-15 17:23:35 +0530

    Create cube.py

---
+++ codekata/absolute_beginner/cube.py
@@ -0,0 +1,13 @@
+# cube of a number
+# get the imput number from user
+n=int(input())
+# define the cube function
+def cube():
+# cube formula is storen in c
+  c=n*n*n
+# return the c value
+  return c
+# call the cube function and store it in number
+number=cube()
+# print the number as output
+print(number)
