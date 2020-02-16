37d8e71749bde0fa3f68f3219ead9ffb79ab9aea
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-16 08:35:37 +0530

    Create area_of_the_rectangle.py

---
+++ codekata/absolute_beginner/area_of_the_rectangle.py
@@ -0,0 +1,14 @@
+# area of the rectangle
+# get length and breadth as input from user
+length=int(input())
+breadth=int(input())
+# define area function
+def area():
+# area of the rectangle formula
+	c=length*breadth
+# return c value
+	return c
+# call area function and store it in result
+result=area()
+# print result value
+print(result)
