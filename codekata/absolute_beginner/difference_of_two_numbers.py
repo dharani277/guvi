2b37c30474ad7b5bc9ccab4c80d0879b621ec741
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-16 14:55:16 +0530

    Create difference_of_two_numbers.py

---
+++ codekata/absolute_beginner/difference_of_two_numbers.py
@@ -0,0 +1,13 @@
+# difference of two numbers
+# get two inputs from user in a single line
+a,b=list(map(int,input().split()))
+# define difference function
+def difference():
+# perform sub operation
+  c=b-a
+# return c value
+  return c
+# call difference function and store it in out
+out=difference()
+# print out value
+print(out)
