4fac58bcfb7eca94cefd0de6af8e6b69aa4c1afa
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-16 23:19:07 +0530

    Update circumference_of_a_circle.py

--- codekata/absolute_beginner/circumference_of_a_circle.py
+++ codekata/absolute_beginner/circumference_of_a_circle.py
@@ -1,14 +1,22 @@
+
 # circumference of a circle
 # get the input radius from user
 radius=float(input())
 # define the function as circumference
 def circumference():
+# if condition check whether the radius is positive or not
+  if(radius>0):
 # circumference of a circle formula
-  c=2*22/7*radius
+    c=2*22/7*radius
 # roundoff to 2nd decimal place
-  d=round(c,2)
+    d=round(c,2)
 # return the value of d
-  return d
+    return d
+# else condtion check whether the radius is negative
+  else:
+    d="Error"
+# return the value of d
+    return d
 # call the circumference function and store the output
 output=circumference()
 # print the output
