81973c92ad8fb6b9cbf7bef69c5b7ad5d7c66b1c
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-16 15:44:18 +0530

    Create sum_of_the_first_K_natural_number.py

---
+++ codekata/absolute_beginner/sum_of_the_first_K_natural_number.py
@@ -0,0 +1,17 @@
+# sum of the first k natural number
+# get input n from user
+n=int(input())
+# define natural function
+def natural():
+# initialize c is equal to zero
+  sum1=0
+# range from 1 to n using for loop
+  for i in range(1,n+1):
+# sum of first k natural numbers
+    sum=sum+i
+# return sum value
+  return sum
+# call the natural function and store it in output
+output=natural()
+# print output value
+print(output)
