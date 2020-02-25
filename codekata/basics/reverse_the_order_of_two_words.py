d2693bf4347a67656cce8d303b43d6c81d136ae3
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-25 21:33:42 +0530

    Delete reverse_the_order_of_two_words.py

--- codekata/basics/reverse_the_order_of_two_words.py
+++
@@ -1,8 +0,0 @@
-# Given a string S consisting of 2 words reverse the order of two words 
-# get input N from user
-N=input()
-c=(N.split())
-# reverse the order and join extra space
-output=' '.join(c[-1:-3:-1])
-# print output value
-print(output)
