cc278c366b8aa5864caf05a144ec5f8077db3b1c
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-26 01:15:56 +0530

    Create bitwise_NOT.py

---
+++ codekata/bitwise/bitwise_NOT.py
@@ -0,0 +1,9 @@
+# print the bitwise not
+# get input N from user
+N=int(input())
+# if condition check the range of N
+if(N<=10000):
+# negation operator is used for bitwise not
+ c=~N
+# print c value
+ print(c)
