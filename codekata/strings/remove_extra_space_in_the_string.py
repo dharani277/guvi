d44bde0580e0e9e55ff976297171907c0f5f9811
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-26 23:26:26 +0530

    Create remove_extra_space_in_the_string.py

---
+++ codekata/strings/remove_extra_space_in_the_string.py
@@ -0,0 +1,8 @@
+# Given a sequence of strings remove extra spaces in the string
+# get input from user
+s=input()
+c=s.split()
+# remove extra space and join the string
+r=' '.join(c)
+# print r value
+print(r)
