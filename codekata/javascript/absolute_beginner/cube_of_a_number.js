e46bb4717f348649eabc2ae1dded8c6bdb16d597
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-27 23:19:12 +0530

    Create cube_of_a_number.js

---
+++ codekata/javascript/absolute_beginner/cube_of_a_number.js
@@ -0,0 +1,8 @@
+// cube of a number
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+inp.on("line", (data) => {
+	console.log(parseInt(data)**3)
+});
