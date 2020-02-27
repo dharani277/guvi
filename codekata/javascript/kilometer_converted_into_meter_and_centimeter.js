11e54ae7f3ca0e7c6514bba9ca6e5d33d42bb6ad
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-27 23:23:17 +0530

    Delete kilometer_converted_into_meter_and_centimeter.js

--- codekata/javascript/kilometer_converted_into_meter_and_centimeter.js
+++
@@ -1,9 +0,0 @@
-// given a number A in Kilometers. Convert this into B: Meters and C: Centi-Metres
-const readline = require('readline');
-const inp = readline.createInterface({
-  input: process.stdin
-});
-inp.on("line", (number) => {
-	console.log(parseInt(number)*1000);
-    console.log(parseInt(number)*100000);
-});
