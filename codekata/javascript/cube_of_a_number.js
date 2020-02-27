e0e86a3f16e9e8d317d5c894351154e788f3d5e3
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-27 23:20:26 +0530

    Delete cube_of_a_number.js

--- codekata/javascript/cube_of_a_number.js
+++
@@ -1,8 +0,0 @@
-// cube of a number
-const readline = require('readline');
-const inp = readline.createInterface({
-  input: process.stdin
-});
-inp.on("line", (data) => {
-	console.log(parseInt(data)**3)
-});
