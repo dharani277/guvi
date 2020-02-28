3c0beccc0f1321c3c8ad8128101aee903ceb2000
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-29 00:41:13 +0530

    Create reverse_natural_number_from_n_to_1.js

---
+++ javascript/looping/reverse_natural_number_from_n_to_1.js
@@ -0,0 +1,8 @@
+// Write a program to print all natural numbers in reverse (from n to 1). - using while loop 
+var natural=prompt("enter the number");
+var i=natural;
+while(i>1)
+{
+    console.log(i);
+    i=i-1;
+}
