918c5e468d9131ba2b902564837086fed18184c1
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-28 23:53:24 +0530

    Create natural_number_from_1_to_n.js

---
+++ javascript/looping/natural_number_from_1_to_n.js
@@ -0,0 +1,8 @@
+// Write a program to print all natural numbers from 1 to n. - using while loop
+var natural=prompt("enter the number");
+var i=1;
+while(i<natural)
+{
+ console.log(i);
+ i=i+1;
+ }
