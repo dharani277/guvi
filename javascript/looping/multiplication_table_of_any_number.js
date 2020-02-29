f099f6571613e43563f295b567f7fd2511505d54
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-29 07:03:44 +0530

    Create multiplication_table_of_any_number.js

---
+++ javascript/looping/multiplication_table_of_any_number.js
@@ -0,0 +1,7 @@
+// Write a program to print multiplication table of any number
+var number=prompt("enter the number");
+for(var i=1;i<=10;i++)
+{
+    var value=number*i;
+    console.log(number,"*",i,"=",value);
+}
