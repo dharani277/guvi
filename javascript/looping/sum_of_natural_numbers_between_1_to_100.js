789728c3f09b2063e7a8bd69e668d641bc98dd1a
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-02-29 06:33:46 +0530

    Create sum_of_natural_numbers_between_1_to_100.js

---
+++ javascript/looping/sum_of_natural_numbers_between_1_to_100.js
@@ -0,0 +1,8 @@
+// Write a program to find sum of all natural numbers between 1 to n
+var natural=prompt("enter the number"); 
+var sum=0
+for(var i=1;i<=natural;i++)
+{
+sum=sum+i;
+}
+console.log(sum);
