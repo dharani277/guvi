87ef4ff87a30305da7469c3427c9fccfa6ccb80a
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-03-06 00:01:59 +0530

    Create first_3_multiples.js

---
+++ codekata/javascript/absolute_beginner/first_3_multiples.js
@@ -0,0 +1,11 @@
+// Print the First 3 multiples of the given number "N"
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=parseInt(n[0].split(" "));
+  console.log(n[0]+" "+(2*n[0])+" "+(3*n[0]));
+});
