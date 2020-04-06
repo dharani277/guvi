ca2d68543d190a619619b2482c92511e2bf9b2ae
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-04-06 17:35:11 +0530

    Create print_the_digit_with_maximum_frequency.js

---
+++ codekata/javascript/numbers/print_the_digit_with_maximum_frequency.js
@@ -0,0 +1,26 @@
+// You are given an array of digits. Your task is to print the digit with maximum frequency
+const readline=require('readline');
+const inp=readline.createInterface({
+    input:process.stdin
+});
+const userInput=[];
+inp.on("line",(data)=>{
+    userInput.push(data);
+});
+inp.on("close",()=>{
+     var a=userInput[0];
+     var b=userInput[1].split(" ");
+     var count=0;
+     var x=[];
+     for(var i=0;i<b.length;i++)
+     {
+         for(var j=i+1;j<b.length;j++)
+         {
+             if(b[i]==b[j])
+             x.push(b[i]);
+             count=count+1;
+         }
+     }
+     if(count>=1)
+     console.log(x[0]);
+});
