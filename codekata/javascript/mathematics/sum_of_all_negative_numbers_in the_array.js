e4df32d0360268d898f41fed1f3820972e2e8c81
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-04-07 16:30:23 +0530

    Create sum_of_all_negative_numbers_in the_array.js

---
+++ codekata/javascript/mathematics/sum_of_all_negative_numbers_in the_array.js
@@ -0,0 +1,23 @@
+// Given a number N and an array of N integers, find the sum of all the negative numbers in the array
+const readline=require('readline');
+const inp=readline.createInterface({
+    input:process.stdin
+});
+const userInput=[];
+inp.on("line",(data)=>{
+    userInput.push(data);
+});
+inp.on("close",()=>{
+    var a=userInput[0];
+    var b=userInput[1].split(" ");
+    var sum=0;
+    for(var i=0;i<a;i++)
+    {
+            if(b[i]<0)
+            {
+            sum=sum+parseInt(b[i])
+            }
+        }
+    console.log(sum)
+ 
+});
