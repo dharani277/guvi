93b29e9f6c2101674066449e93bde8e74445d7fb
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-04-10 18:11:44 +0530

    Create string_without_using_semicolon.js

---
+++ codekata/javascript/strings/string_without_using_semicolon.js
@@ -0,0 +1,8 @@
+// Given a string S, print it without using semicolon in your program
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin
+});
+inp.on("line",(data)=>{
+  console.log(data)
+ });
