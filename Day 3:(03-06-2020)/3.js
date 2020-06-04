942dc17b2ea69b197a8e8d08c0708874f7fd5fd6
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-06-04 23:03:09 +0530

    Update 3.js

--- Day 3:(03-06-2020)/3.js
+++ Day 3:(03-06-2020)/3.js
@@ -2,62 +2,67 @@
 
 // 3.1. if the give number is even
 
-// declare a set of values to an array
-var array=[1,2,3,4,5,6,7,8,9,10];
-var result=[];
+// declare the variable name as number with value
+var number=26;
+var result;
 // declare function with the name of evenNumber by using call back 
-function evenNumber(array,callback){ 
-    for(var i=0;i<array.length;i++)
-    {
-        if(array[i]%2===0)
-    result.push(array[i]);
-    }
+function evenNumber(number,callback){ 
+        if(number%2===0)
+        result="even";
+        else
+        result="odd";
     callback(); 
     }
     function even(){ 
    console.log(result); 
    } 
- evenNumber(array,even);     
+ evenNumber(number,even);     
    
 // 3.2. if the given number is prime
 
-// declare a set of values to an array
-var array = [2,3,4,5,6,7,8,9,10,11,12];
+// declare the variable name as number with value
+var number=3;
 var result;
 // declare function with the name of primeNumber by using call back
-function primeNumber(array,callback){ 
- result= array.filter((number) => {
-  for (var i = 2; i <= Math.sqrt(number); i++) 
-  {
-    if (number % i === 0) 
-    return 0;
+function primeNumber(number,callback){ 
+
+  for(var i=2;i<=number;i++)
+  { 
+    var not=1;
+      for(var j=2;j<=i;j++)
+      {
+        if(i%j===0 && j!==i){
+          not=0;
+          result="not a prime";
+        }
+}
+  if(not===1){
+    result="prime";
+      }
   }
-  return 1;
-});
-    callback(); 
+  callback();
     }
     function prime(){ 
    console.log(result); 
    } 
- primeNumber(array,prime);     
+ primeNumber(number,prime);        
    
 // 3.3. if the number is palindrome
 
-// declare a set of strings to an array
-var array=["i","love", "my","dad"];
-var palindrome;
-// declare function with the name of primeNumber by using call back
-function primeNumber(array,callback){ 
-palindrome = array.filter((number)=>{
-var result=number.split("").reverse().join('');
-    for(var i=0;i<array.length;i++){
-    if(result==array[i])
-    return result;
-}
-  });
+// declare the variable name as string
+var string="dad";
+var result;
+// declare function with the name of palindrome by using call back
+function palindrome(string,callback)
+{ 
+result=string.split("").reverse().join("");
+    if(result==string)
+    result="palindrome";
+    else
+    result="not a palindrome";
     callback(); 
     }
     function pal(){ 
-   console.log(palindrome); 
+   console.log(result); 
    } 
- primeNumber(array,pal);     
+ palindrome(string,pal);     
